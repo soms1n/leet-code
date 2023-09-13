039efe82d0d218bd63f64f71ab60b59eb5861161
Somsin <privetdruk.inc@gmail.com>
2023-09-13 21:13:22 +0300

    1920. Build Array from Permutation

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_1920_BuildArrayFromPermutation.java
@@ -0,0 +1,13 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_1920_BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
