51001fe9686f422403a575a3bdd2f153b2cebb3d
Somsin <privetdruk.inc@gmail.com>
2023-09-14 09:21:59 +0300

    1512. Number of Good Pairs
    Space: O(n^2)
    Time: O(n)

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_1512_NumberOfGoodPairs.java
@@ -0,0 +1,17 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_1512_NumberOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int counter = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
