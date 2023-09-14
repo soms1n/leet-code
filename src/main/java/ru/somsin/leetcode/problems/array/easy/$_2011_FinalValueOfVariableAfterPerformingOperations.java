02b119c7452a0414ee3235c4f5ac47f329d9f7b0
Somsin <privetdruk.inc@gmail.com>
2023-09-14 09:15:33 +0300

    2011. Final Value of Variable After Performing Operations
    Space: O(1)
    Time: O(n)

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_2011_FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,13 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_2011_FinalValueOfVariableAfterPerformingOperations {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+
+        for (String operation : operations) {
+            x = operation.contains("-") ? x - 1 : x + 1;
+        }
+
+        return x;
+    }
+}
