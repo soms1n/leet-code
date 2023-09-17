faa24f72f5ee6b57cc3e82a7b8adb8c82177a324
Somsin <privetdruk.inc@gmail.com>
2023-09-18 00:06:22 +0300

    905. Sort Array By Parity
    Space: O(1)
    Time: O(n)

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_905_SortArrayByParity.java
@@ -0,0 +1,21 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_905_SortArrayByParity {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            if (nums[left] % 2 == 0) {
+                left++;
+            } else if (nums[right] % 2 == 0) {
+                int temp = nums[left];
+                nums[left++] = nums[right];
+                nums[right--] = temp;
+            } else {
+                right--;
+            }
+        }
+
+        return nums;
+    }
+}
