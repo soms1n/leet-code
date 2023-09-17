16fcbce7a40a2af21a5cdd7a2f1485c582359bb6
Somsin <privetdruk.inc@gmail.com>
2023-09-17 23:55:12 +0300

    961. N-Repeated Element in Size 2N Array
    Space: O(1)
    Time: O(n)

--- src/main/java/ru/somsin/leetcode/problems/array/easy/$_961_NRepeatedElementInSize2NArray.java
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_961_NRepeatedElementInSize2NArray.java
@@ -1,21 +1,13 @@
 package ru.somsin.leetcode.problems.array.easy;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class $_961_NRepeatedElementInSize2NArray {
     public int repeatedNTimes(int[] nums) {
-        Map<Integer, Integer> numbers = new HashMap<>();
-        for (int num : nums) {
-            numbers.put(num, numbers.getOrDefault(num, 0) + 1);
-        }
-
-        for (int number : numbers.keySet()) {
-            if (numbers.get(number) > 1) {
-                return number;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] || nums[i] == nums[i - 2]) {
+                return nums[i];
             }
         }
 
-        throw new RuntimeException();
+        return nums[0];
     }
 }
