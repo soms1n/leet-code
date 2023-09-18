1ffd48ba4b2769dd6c39d83a00d6dd725deefbd9
Somsin <privetdruk.inc@gmail.com>
2023-09-18 23:40:33 +0300

    1822. Sign of the Product of an Array
    Time: O(n)
    Space: O(1)

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_1822_SignOfTheProductOfAnArray.java
@@ -0,0 +1,17 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_1822_SignOfTheProductOfAnArray {
+    public int arraySign(int[] nums) {
+        int result = 1;
+
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            }
+
+            result *= num > 0 ? 1 : -1;
+        }
+
+        return result;
+    }
+}
