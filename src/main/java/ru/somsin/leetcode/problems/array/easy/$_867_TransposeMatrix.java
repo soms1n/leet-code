74cd2f30c6d26f030a05b6d931252b745c01ae09
Somsin <privetdruk.inc@gmail.com>
2023-09-18 19:09:57 +0300

    867. Transpose Matrix
    Time: O(n * m)
    Space: O(n * m)

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_867_TransposeMatrix.java
@@ -0,0 +1,18 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_867_TransposeMatrix {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+}
