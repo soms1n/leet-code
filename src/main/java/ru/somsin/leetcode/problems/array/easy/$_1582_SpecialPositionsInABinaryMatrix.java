a56f91dc946d6c976a4004a4977ffff6dba21997
Somsin <privetdruk.inc@gmail.com>
2023-09-21 10:02:01 +0300

    1582. Special Positions in a Binary Matrix
    Time: O(n * m)
    Space: O(n)

--- src/main/java/ru/somsin/leetcode/problems/array/easy/$_1582_SpecialPositionsInABinaryMatrix.java
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_1582_SpecialPositionsInABinaryMatrix.java
@@ -1,40 +1,26 @@
 package ru.somsin.leetcode.problems.array.easy;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class $_1582_SpecialPositionsInABinaryMatrix {
     public int numSpecial(int[][] mat) {
-        Map<Integer, Integer> ones = new HashMap<>();
+        int[] rows = new int[mat.length], columns = new int[mat[0].length];
 
         for (int row = 0; row < mat.length; row++) {
             for (int column = 0; column < mat[row].length; column++) {
                 if (mat[row][column] == 1) {
-                    int counter = ones.getOrDefault(row, 0) + 1;
-                    ones.put(row, counter);
-
-                    if (counter > 1) {
-                        break;
-                    }
+                    rows[row]++;
+                    columns[column]++;
                 }
             }
         }
 
         int counter = 0;
 
-        for (int column = 0; column < mat[0].length; column++) {
-            int numbers = 0;
-            for (int[] rows : mat) {
-                if (rows[column] == 1) {
-                    numbers = ones.getOrDefault(column, 0) + 1;
-                    ones.put(column, numbers);
-
-                    if (numbers > 2) {
-                        break;
-                    }
+        for (int row = 0; row < mat.length; row++) {
+            for (int column = 0; column < mat[row].length; column++) {
+                if (mat[row][column] == 1 && rows[row] == 1 && columns[column] == 1) {
+                    counter++;
                 }
             }
-            counter += numbers == 2 ? 1 : 0;
         }
 
         return counter;
