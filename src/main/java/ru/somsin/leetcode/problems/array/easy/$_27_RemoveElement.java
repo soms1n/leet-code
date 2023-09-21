b92068e20917ade5c3cb4e2f6d193f8176642819
Somsin <privetdruk.inc@gmail.com>
2023-09-21 10:59:46 +0300

    27. Remove Element
    Time: O(n)
    Space: O(1)

---
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_27_RemoveElement.java
@@ -0,0 +1,15 @@
+package ru.somsin.leetcode.problems.array.easy;
+
+public class $_27_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[left++] = nums[i];
+            }
+        }
+
+        return left;
+    }
+}
