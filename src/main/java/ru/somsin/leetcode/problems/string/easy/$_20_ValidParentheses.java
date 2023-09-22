a96e241d0ed8d375af80d0758f98d18a0e52af08
Somsin <privetdruk.inc@gmail.com>
2023-09-22 21:31:43 +0300

    20. Valid Parentheses
    Time: O(n)
    Space: O(n / 2)

--- src/main/java/ru/somsin/leetcode/problems/string/easy/$_20_ValidParentheses.java
+++ src/main/java/ru/somsin/leetcode/problems/string/easy/$_20_ValidParentheses.java
@@ -1,45 +1,28 @@
 package ru.somsin.leetcode.problems.string.easy;
 
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
 
 public class $_20_ValidParentheses {
-    private final Map<Character, Character> signs = new HashMap<>();
-
-    {
-        signs.put('(', ')');
-        signs.put('[', ']');
-        signs.put('{', '}');
-    }
-
-    private final Set<Character> end = new HashSet<>();
-
-    {
-        end.add(')');
-        end.add(']');
-        end.add('}');
-    }
-
     public boolean isValid(String s) {
-        if (end.contains(s.charAt(0)) || s.length() % 2 != 0) {
+        char firstSign = s.charAt(0);
+
+        if ((firstSign != '(' && firstSign != '{' && firstSign != '[') || s.length() % 2 != 0) {
             return false;
         }
 
         Deque<Character> stack = new LinkedList<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char sign = s.charAt(i);
-
-            if (end.contains(sign)) {
-                if (stack.peek() == null || sign != signs.get(stack.pop())) {
-                    return false;
+        for (char sign : s.toCharArray()) {
+            switch (sign) {
+                case '(' -> stack.push(')');
+                case '{' -> stack.push('}');
+                case '[' -> stack.push(']');
+                default -> {
+                    if (stack.isEmpty() || stack.pop() != sign) {
+                        return false;
+                    }
                 }
-            } else {
-                stack.push(sign);
             }
         }
 
