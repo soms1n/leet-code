027db31fe0efbf9c474202d70b5701633c3ef07d
Somsin <privetdruk.inc@gmail.com>
2023-09-22 12:19:29 +0300

    13. Roman to Integer
    Time: O(n)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/string/easy/$_13_RomanToInteger.java
+++ src/main/java/ru/somsin/leetcode/problems/string/easy/$_13_RomanToInteger.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class $_13_RomanToInteger {
-    public int romanToInt(String s) {
-        Map<Character, Integer> values = new HashMap<>();
+    private final Map<Character, Integer> values = new HashMap<>();
+
+    {
         values.put('I', 1);
         values.put('V', 5);
         values.put('X', 10);
@@ -13,57 +14,17 @@ public class $_13_RomanToInteger {
         values.put('C', 100);
         values.put('D', 500);
         values.put('M', 1000);
+    }
 
-        int value = 0;
+    public int romanToInt(String s) {
+        int result = 0, value = 0, prev = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            char nextCh = i + 1 == s.length() ? 0 : s.charAt(i + 1);
-
-            switch (ch) {
-                case 'I' -> {
-                    switch (nextCh) {
-                        case 'V' -> {
-                            value += 4;
-                            i++;
-                        }
-                        case 'X' -> {
-                            value += 9;
-                            i++;
-                        }
-                        default -> value += values.get(ch);
-                    }
-                }
-                case 'X' -> {
-                    switch (nextCh) {
-                        case 'L' -> {
-                            value += 40;
-                            i++;
-                        }
-                        case 'C' -> {
-                            value += 90;
-                            i++;
-                        }
-                        default -> value += values.get(ch);
-                    }
-                }
-                case 'C' -> {
-                    switch (nextCh) {
-                        case 'D' -> {
-                            value += 400;
-                            i++;
-                        }
-                        case 'M' -> {
-                            value += 900;
-                            i++;
-                        }
-                        default -> value += values.get(ch);
-                    }
-                }
-                default -> value += values.get(ch);
-            }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            value = values.get(s.charAt(i));
+            result += value < prev ? -value : value;
+            prev = value;
         }
 
-        return value;
+        return result;
     }
 }
