42ee1af183681b10e863eb6024221d18f4853b36
Somsin <privetdruk.inc@gmail.com>
2023-10-08 16:45:30 +0300

    48. Rotate Image
    Time: O(n^2)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/array/easy/$_48_RotateImage.java
+++ src/main/java/ru/somsin/leetcode/problems/array/easy/$_48_RotateImage.java
@@ -3,22 +3,22 @@ package ru.somsin.leetcode.problems.array.easy;
 public class $_48_RotateImage {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int steps = n * n, left = 0, right = n - 1, temp = matrix[0][0];
 
-        for (int step = 0, row = 0, column = 0, offset = 0; step < steps - 1; step++) {
-            int value = temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
 
-            int tempRow = row;
-            row = left + column;
-            column = right - tempRow;
+        int right = n - 1;
 
-            temp = matrix[row][column];
-            matrix[row][column] = value;
-
-            if (temp == value) {
-                row = ++offset;
-                column = 0;
-                temp = matrix[row][column];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][right - j];
+                matrix[i][right - j] = temp;
             }
         }
     }
