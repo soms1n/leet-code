bfb185a5126ed56413c1b0518769a9620b72a2db
Somsin <privetdruk.inc@gmail.com>
2023-10-30 22:01:23 +0300

    344. Reverse String
    Time: O(n)
    Space: O(1)

---
+++ src/main/java/ru/somsin/leetcode/problems/easy/$_344_ReverseString.java
@@ -0,0 +1,13 @@
+package ru.somsin.leetcode.problems.easy;
+
+public class $_344_ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+
+        while (left <= right) {
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+        }
+    }
+}
