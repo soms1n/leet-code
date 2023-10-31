0d068b5d4b9b1b619f583753f4abcd8831a5d2bc
Somsin <privetdruk.inc@gmail.com>
2023-10-31 18:59:43 +0300

    389. Find the Difference
    Time: O(n)
    Space: O(1)

---
+++ src/main/java/ru/somsin/leetcode/problems/easy/$_389_FindTheDifference.java
@@ -0,0 +1,29 @@
+package ru.somsin.leetcode.problems.easy;
+
+import java.util.Objects;
+
+public class $_389_FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        if (Objects.equals(s, "")) {
+            return t.charAt(0);
+        }
+
+        int[] sChars = new int[26];
+        int[] tChars = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            sChars[s.charAt(i) - 97]++;
+            tChars[t.charAt(i) - 97]++;
+        }
+
+        tChars[t.charAt(t.length() - 1) - 97]++;
+
+        for (int i = 0; i < sChars.length; i++) {
+            if (sChars[i] != tChars[i]) {
+                return (char) (i + 97);
+            }
+        }
+
+        throw new RuntimeException();
+    }
+}
