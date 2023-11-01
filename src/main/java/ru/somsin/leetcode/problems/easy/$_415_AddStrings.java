d1de72076e04c006630f22f643f94031f06cce79
Somsin <privetdruk.inc@gmail.com>
2023-11-01 21:43:43 +0300

    415. Add Strings
    Time: O(n)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/easy/$_415_AddStrings.java
+++ src/main/java/ru/somsin/leetcode/problems/easy/$_415_AddStrings.java
@@ -2,41 +2,27 @@ package ru.somsin.leetcode.problems.easy;
 
 public class $_415_AddStrings {
     public String addStrings(String num1, String num2) {
-        String result = "";
-        boolean add = false;
+        StringBuilder result = new StringBuilder();
 
-        for (int index = Math.max(num1.length(), num2.length()) - 1, indexNum1 = num1.length() - 1, indexNum2 = num2.length() - 1; index >= 0; index--, indexNum1--, indexNum2--) {
-            int total = 0;
+        int indexNum1 = num1.length() - 1, indexNum2 = num2.length() - 1;
+        int additinalValue = 0;
 
-            if (indexNum1 >= 0 && indexNum2 >= 0) {
-                total = defineNumber(num1, indexNum1) + defineNumber(num2, indexNum2);
-            } else if (indexNum1 >= 0) {
-                total = defineNumber(num1, indexNum1);
-            } else if (indexNum2 >= 0) {
-                total = defineNumber(num2, indexNum2);
+        while (indexNum1 >= 0 || indexNum2 >= 0 || additinalValue > 0) {
+            int value = additinalValue;
+
+            if (indexNum1 >= 0) {
+                value += num1.charAt(indexNum1--) - '0';
             }
 
-            if (add) {
-                add = false;
-                total++;
+            if (indexNum2 >= 0) {
+                value += num2.charAt(indexNum2--) - '0';
             }
 
-            if (total >= 10) {
-                result = String.valueOf(total).charAt(1) + result;
-                add = true;
-            } else {
-                result = total + result;
-            }
+            result.append(value % 10);
+
+            additinalValue = value >= 10 ? 1 : 0;
         }
 
-        if (add) {
-            result = "1" + result;
-        }
-
-        return result;
-    }
-
-    private int defineNumber(String number, int index) {
-        return (int) number.charAt(index) - 48;
+        return result.reverse().toString();
     }
 }
