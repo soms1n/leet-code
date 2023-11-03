005451d3023b5b5739366332bcbda3dd917cd5ea
Somsin <privetdruk.inc@gmail.com>
2023-11-03 13:46:38 +0300

    459. Repeated Substring Pattern
    Time: O(n)
    Space: O(1)
    Description: На половину рабочая версия

---
+++ src/main/java/ru/somsin/leetcode/problems/easy/$_459_RepeatedSubstringPattern.java
@@ -0,0 +1,28 @@
+package ru.somsin.leetcode.problems.easy;
+
+public class $_459_RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        int length = s.length();
+
+        if (length % 2 != 0) {
+            return false;
+        }
+
+        char firstChar = s.charAt(0);
+        int right = 0;
+
+        for (int index = 1; index < length; index++) {
+            if (firstChar == s.charAt(index)) {
+                right = index;
+            }
+        }
+
+        for (int left = 0, offset = right; left + offset < length; left++, right++) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
