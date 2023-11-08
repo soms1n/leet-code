f20c24c6bd7633ff86e80e83913ab94647b8a36b
Somsin <privetdruk.inc@gmail.com>
2023-11-08 14:34:45 +0300

    110. Balanced Binary Tree
    Time: O(n)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/easy/$_110_BalancedBinaryTree.java
+++ src/main/java/ru/somsin/leetcode/problems/easy/$_110_BalancedBinaryTree.java
@@ -2,7 +2,7 @@ package ru.somsin.leetcode.problems.easy;
 
 public class $_110_BalancedBinaryTree {
     public boolean isBalanced(TreeNode root) {
-        return root == null || Math.abs(dfs(root.left, 0) - dfs(root.right, 0)) < 2;
+        return root == null || dfs(root, 0) != -1;
     }
 
     private int dfs(TreeNode node, int depth) {
@@ -12,6 +12,14 @@ public class $_110_BalancedBinaryTree {
 
         depth++;
 
-        return Math.max(dfs(node.left, depth), dfs(node.right, depth));
+        int depthLeft = dfs(node.left, depth);
+        int depthRight = dfs(node.right, depth);
+        int depthNode = Math.abs(Math.abs(depthLeft - depthRight));
+
+        if (depthLeft == -1 || depthRight == -1 || depthNode > 1) {
+            return -1;
+        }
+
+        return Math.max(depthLeft, depthRight);
     }
 }
