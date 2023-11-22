ba56457cd871d670a995baef6518e67a15df8784
Somsin <privetdruk.inc@gmail.com>
2023-11-22 19:59:54 +0300

    56. Merge Intervals
    Time: O(nlogn)
    Space: O(n)

--- src/main/java/ru/somsin/leetcode/problems/$_56_MergeIntervals.java
+++ src/main/java/ru/somsin/leetcode/problems/$_56_MergeIntervals.java
@@ -2,52 +2,21 @@ package ru.somsin.leetcode.problems;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.LinkedList;
 
 public class $_56_MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        int rows = intervals.length;
-
-        int size = 0, left = -1, right = -1;
-
         Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+        LinkedList<int[]> result = new LinkedList<>();
 
-        int[] temp = new int[rows * 2];
-
-        for (int row = 0; row < rows; row++) {
-            if (left == -1) {
-                left = intervals[row][0];
-                right = intervals[row][1];
-            }
-
-            if (intervals[row][0] <= right) {
-                if (intervals[row][1] > right) {
-                    right = intervals[row][1];
-                }
-
-                if (intervals[row][0] <= left) {
-                    left = intervals[row][0];
-                }
+        for (int[] interval : intervals) {
+            if (result.isEmpty() || interval[0] > result.getLast()[1]) {
+                result.add(interval);
             } else {
-                temp[size++] = left;
-                temp[size++] = right;
-
-                left = intervals[row][0];
-                right = intervals[row][1];
-            }
-
-            if (row == rows - 1) {
-                temp[size++] = left;
-                temp[size++] = right;
+                result.getLast()[1] = Math.max(result.getLast()[1], interval[1]);
             }
         }
 
-        int[][] result = new int[size / 2][2];
-
-        for (int i = 0, j = 0; i < size; i += 2, j++) {
-            result[j][0] = temp[i];
-            result[j][1] = temp[i + 1];
-        }
-
-        return result;
+        return result.toArray(new int[result.size()][]);
     }
 }
