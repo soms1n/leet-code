77bcf5dbbb837755a24f3fcad77f66f01a70f8f1
Somsin <privetdruk.inc@gmail.com>
2023-11-29 14:23:37 +0300

    33. Search in Rotated Sorted Array
    Time: O(logn)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/$_33_SearchInRotatedSortedArray.java
+++ src/main/java/ru/somsin/leetcode/problems/$_33_SearchInRotatedSortedArray.java
@@ -1,26 +1,33 @@
 package ru.somsin.leetcode.problems;
 
-import java.util.Arrays;
-
 public class $_33_SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
         int length = nums.length;
 
         int left = 0, right = length - 1;
 
-        if (target <= nums[length - 1] && target < nums[0]) {
-            left = length / 2;
-            right = length - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
 
-            if (nums[left] > target) {
-                left++;
+            if (nums[mid] == target) {
+                return mid;
             }
-        } else if (target > nums[length - 1]) {
-            right = length / 2;
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
 
-        int result = Arrays.binarySearch(nums, left, right + 1, target);
-
-        return result < 0 ? -1 : result;
+        return nums[left - 1] == target ? left : -1;
     }
 }
