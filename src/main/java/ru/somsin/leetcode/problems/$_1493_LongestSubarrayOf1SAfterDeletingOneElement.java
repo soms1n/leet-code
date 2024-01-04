f99fd7aff948dfbadb5273a8b411fff472764a66
Somsin <privetdruk.inc@gmail.com>
2024-01-04 18:55:32 +0300

    1493. Longest Subarray of 1's After Deleting One Element
    Time: O(n)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/$_1493_LongestSubarrayOf1SAfterDeletingOneElement.java
+++ src/main/java/ru/somsin/leetcode/problems/$_1493_LongestSubarrayOf1SAfterDeletingOneElement.java
@@ -2,10 +2,9 @@ package ru.somsin.leetcode.problems;
 
 public class $_1493_LongestSubarrayOf1SAfterDeletingOneElement {
     public int longestSubarray(int[] nums) {
-        int result = 0, counter = 0, counterZero = 0;
+        int result = 0, counter = 0;
 
         for (int left = 0, right = 0; right < nums.length; right++) {
-            counterZero += nums[right] == 0 ? 1 : 0;
             counter += nums[right] == 0 ? 0 : 1;
 
             if (right - left + 1 - counter > 1) {
@@ -15,6 +14,6 @@ public class $_1493_LongestSubarrayOf1SAfterDeletingOneElement {
             result = Math.max(result, right - left);
         }
 
-        return counterZero == 0 ? nums.length - 1 : counterZero == nums.length ? 0 : result;
+        return result == nums.length ? result - 1 : result;
     }
 }
