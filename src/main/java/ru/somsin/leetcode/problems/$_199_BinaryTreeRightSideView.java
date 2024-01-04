174aebff4b9b2d8c59ee1d76de902c08defc26da
Somsin <privetdruk.inc@gmail.com>
2024-01-04 19:42:03 +0300

    199. Binary Tree Right Side View
    Time: O(n)
    Space: O(n)

--- src/main/java/ru/somsin/leetcode/problems/$_199_BinaryTreeRightSideView.java
+++ src/main/java/ru/somsin/leetcode/problems/$_199_BinaryTreeRightSideView.java
@@ -1,38 +1,27 @@
 package ru.somsin.leetcode.problems;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class $_199_BinaryTreeRightSideView {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        Set<Integer> deep = new HashSet<>();
 
-        dfs(root, result, deep, 0);
+        dfs(root, result, 0);
 
         return result;
     }
 
-    public void dfs(TreeNode node, List<Integer> result, Set<Integer> deep, int level) {
+    public void dfs(TreeNode node, List<Integer> result, int level) {
         if (node == null) {
             return;
         }
 
-        if (!deep.contains(level)) {
+        if (level++ == result.size()) {
             result.add(node.val);
-            deep.add(level);
         }
 
-        level++;
-
-        if (node.right != null) {
-            dfs(node.right, result, deep, level);
-        }
-
-        if (node.left != null) {
-            dfs(node.left, result, deep, level);
-        }
+        dfs(node.right, result, level);
+        dfs(node.left, result, level);
     }
 }
