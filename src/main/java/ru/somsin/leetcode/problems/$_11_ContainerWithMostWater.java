1b529233338c2eb8129b5584b68ae54d2cc4946e
Somsin <privetdruk.inc@gmail.com>
2024-01-12 18:01:47 +0300

    11. Container With Most Water
    Time: O(n)
    Space: O(1)

---
+++ src/main/java/ru/somsin/leetcode/problems/$_11_ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+package ru.somsin.leetcode.problems;
+
+public class $_11_ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, area = 0;
+
+        while (left < right) {
+            area = Math.max(area, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return area;
+    }
+}
