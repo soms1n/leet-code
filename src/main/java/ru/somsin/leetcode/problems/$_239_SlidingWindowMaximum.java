148665de17f7b3039b7fe7ae6cddd37cf564b199
Somsin <privetdruk.inc@gmail.com>
2024-01-15 14:16:26 +0300

    239. Sliding Window Maximum
    Time: O(nk)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/$_239_SlidingWindowMaximum.java
+++ src/main/java/ru/somsin/leetcode/problems/$_239_SlidingWindowMaximum.java
@@ -1,27 +1,18 @@
 package ru.somsin.leetcode.problems;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class $_239_SlidingWindowMaximum {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> queue = new LinkedList<>();
+        int windows = nums.length - k + 1;
+        int[] result = new int[windows];
 
-        int[] result = new int[nums.length - k + 1];
-        int size = 0;
+        for (int window = 0; window < windows; window++) {
+            int max = Integer.MIN_VALUE;
 
-        for (int num : nums) {
-            queue.add(num);
-
-            if (queue.size() == k) {
-                Integer max = queue.stream()
-                        .max(Integer::compareTo)
-                        .orElseThrow();
-
-                result[size++] = max;
-
-                queue.pop();
+            for (int index = window; index < window + k; index++) {
+                max = Math.max(max, nums[index]);
             }
+
+            result[window] = max;
         }
 
         return result;
