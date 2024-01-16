be1f7f7f68747733fdc917069c65da0d8e3c397e
Somsin <privetdruk.inc@gmail.com>
2024-01-16 21:34:30 +0300

    240. Search a 2D Matrix II
    Time: O(n + m)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/$_240_SearchA2DMatrixII.java
+++ src/main/java/ru/somsin/leetcode/problems/$_240_SearchA2DMatrixII.java
@@ -2,21 +2,15 @@ package ru.somsin.leetcode.problems;
 
 public class $_240_SearchA2DMatrixII {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int[] values : matrix) {
-            int left = 0, right = values.length - 1;
+        int row = 0, column = matrix[0].length - 1;
 
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-
-                int midValue = values[mid];
-
-                if (midValue == target) {
-                    return true;
-                } else if (midValue > target) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+        while (row < matrix.length && column >= 0) {
+            if (matrix[row][column] == target) {
+                return true;
+            } else if (matrix[row][column] > target) {
+                column--;
+            } else {
+                row++;
             }
         }
 
