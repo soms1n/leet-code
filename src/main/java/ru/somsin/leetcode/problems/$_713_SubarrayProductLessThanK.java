37e17e552b0841df3a1440fa1737f8bc8673be54
Somsin <privetdruk.inc@gmail.com>
2024-01-16 14:00:45 +0300

    713. Subarray Product Less Than K
    Time: O(n)
    Space: O(1)

--- src/main/java/ru/somsin/leetcode/problems/$_713_SubarrayProductLessThanK.java
+++ src/main/java/ru/somsin/leetcode/problems/$_713_SubarrayProductLessThanK.java
@@ -2,26 +2,16 @@ package ru.somsin.leetcode.problems;
 
 public class $_713_SubarrayProductLessThanK {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int result = 0;
+        int result = 0, product = 1;
 
-        for (int left = 0; left < nums.length; left++) {
-            if (nums[left] >= k) {
-                continue;
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            product *= nums[right];
+
+            while (product >= k && left <= right) {
+                product /= nums[left++];
             }
 
-            result++;
-
-            int product = nums[left];
-
-            for (int right = left + 1; right < nums.length; right++) {
-                product *= nums[right];
-
-                if (product >= k) {
-                    break;
-                }
-
-                result++;
-            }
+            result += right - left + 1;
         }
 
         return result;
