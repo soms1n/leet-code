c5af3cabeeca98cbb4be502eb9f097e4fa18739b
Сомсин Дмитрий Александрович <somsin.dmitrii@gmail.com>
2025-04-01 00:04:31 +0300

    160. Intersection of Two Linked Lists

--- src/main/java/ru/somsin/leetcode/problems/$_160_IntersectionOfTwoLinkedLists.java
+++ src/main/java/ru/somsin/leetcode/problems/$_160_IntersectionOfTwoLinkedLists.java
@@ -1,32 +1,14 @@
 package ru.somsin.leetcode.problems;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class $_160_IntersectionOfTwoLinkedLists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> visited = new HashSet<>();
+        ListNode a = headA, b = headB;
 
-        while (headA != null || headB != null) {
-            if (headA != null) {
-                if (visited.contains(headA)) {
-                    return headA;
-                } else {
-                    visited.add(headA);
-                    headA = headA.next;
-                }
-            }
-
-            if (headB != null) {
-                if (visited.contains(headB)) {
-                    return headB;
-                } else {
-                    visited.add(headB);
-                    headB = headB.next;
-                }
-            }
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
 
-        return null;
+        return a;
     }
 }
