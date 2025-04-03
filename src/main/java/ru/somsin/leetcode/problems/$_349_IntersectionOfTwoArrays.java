39e0545e5a48b6c86b9a761a9ad6db46e9159400
Somsin <somsin.dmitrii@gmail.com>
2025-04-03 22:29:41 +0300

    349. Intersection of Two Arrays

---
+++ src/main/java/ru/somsin/leetcode/problems/$_349_IntersectionOfTwoArrays.java
@@ -0,0 +1,28 @@
+package ru.somsin.leetcode.problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class $_349_IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> numbers = new HashSet<>();
+
+        for (int number : nums1) {
+            numbers.add(number);
+        }
+
+        if (numbers.isEmpty()) {
+            return null;
+        }
+
+        Set<Integer> result = new HashSet<>();
+
+        for (int number: nums2) {
+            if (numbers.contains(number)) {
+                result.add(number);
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
