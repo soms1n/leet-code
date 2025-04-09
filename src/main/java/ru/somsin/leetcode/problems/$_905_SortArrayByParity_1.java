7869c59d92ebcb165e98301f5a55a3530d1b4b13
Somsin <somsin.dmitrii@gmail.com>
2025-04-10 00:02:43 +0300

    905. Sort Array By Parity

--- src/main/java/ru/somsin/leetcode/problems/$_905_SortArrayByParity_1.java
+++ src/main/java/ru/somsin/leetcode/problems/$_905_SortArrayByParity_1.java
@@ -5,9 +5,8 @@ public class $_905_SortArrayByParity_1 {
         for (int left = 0, right = 0; right < nums.length; right++) {
             if (nums[right] % 2 == 0) {
                 int temp = nums[left];
-                nums[left] = nums[right];
+                nums[left++] = nums[right];
                 nums[right] = temp;
-                right = left++;
             }
         }
         return nums;
