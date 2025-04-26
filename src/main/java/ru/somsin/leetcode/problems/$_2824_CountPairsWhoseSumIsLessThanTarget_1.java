cfa2396b4ac885f8120f53e181d4cad499f463be
Somsin <somsin.dmitrii@gmail.com>
2025-04-26 19:38:49 +0300

    2824. Count Pairs Whose Sum is Less than Target

---
+++ src/main/java/ru/somsin/leetcode/problems/$_2824_CountPairsWhoseSumIsLessThanTarget_1.java
@@ -0,0 +1,19 @@
+package ru.somsin.leetcode.problems;
+
+import java.util.List;
+
+public class $_2824_CountPairsWhoseSumIsLessThanTarget_1 {
+    public int countPairs(List<Integer> nums, int target) {
+        int result = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
